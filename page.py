94b61463037df2712820e8b6a268df5385a73928
Denis Costa <deniscostadsc@gmail.com>
2011-10-24 06:02:30 -0200

    Adicionando comentários a classe Page.

--- page.py
+++ page.py
@@ -5,6 +5,7 @@ class Page(object):
     def __init__(self):
         self.url='http://bicicletada.org/'
 
+        # Pega a página configurada na URL.
         self.page=urllib.urlopen(self.url)
 
         # Retorna código de retorno HTTP
@@ -12,6 +13,7 @@ class Page(object):
         # http://pt.wikipedia.org/wiki/Hypertext_Transfer_Protocol#C.C3.B3digos_de_retorno
         self.status_code=self.page.getcode()
 
+        # Joga o conteúdo da página nesse atributo.
         self.content=self.page.read()
         
         self.page.close()
