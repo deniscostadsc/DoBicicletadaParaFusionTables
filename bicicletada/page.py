bcef4ce97fd19656aae6424167fb8bf0b34ad83f
Denis Costa <deniscostadsc@gmail.com>
2011-10-29 13:48:15 -0200

    Corrigindo typo no test_csv, e corrigido codificação para rodar os testes.

--- bicicletada/page.py
+++ bicicletada/page.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+#coding: utf-8
+
 import urllib
 
 class Page(object):
--- bicicletada/test_csv.py
+++ bicicletada/test_csv.py
@@ -4,12 +4,11 @@
 import unittest
 from csv import CSV
 
-
 class TestCSV(unittest.TestCase):
 
     def test_if_there_are_3_fields(self):
         csv=CSV()
         self.assertEquals(len(csv.fields), 3)
 
-if __main__ == '__name__':
+if __name__ == '__main__':
     unittest.main()
