7ddfa773d7dd02a6ac5d5f889940c4d34b441038
Denis Costa <deniscostadsc@gmail.com>
2011-10-29 13:24:34 -0200

    Retirado código comentado.

--- bicicletada/csv.py
+++ bicicletada/csv.py
@@ -3,14 +3,3 @@ from page import Page
 class CSV(object):
     def __init__(self):
         self.fields = ['Localização', 'Estado', 'URL']
-    
-    # ver = ''
-    # for linha in s.split('\n'):
-    #     if linha.startswith('<div id="Estados"'):
-    #         ver = True
-    #     if linha.startswith('</div>') and ver == True:
-    #         ver = False
-    #     if ver == True:
-    #         print(linha)
-    #     if ver == False:
-    #         break
