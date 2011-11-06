ece894fd62e5979d73dc4395d59931b95c3aacea
Denis Costa <deniscostadsc@gmail.com>
2011-11-06 15:56:43 -0200

    Refatoração da classe de teste do csv.

--- bicicletada/test_csv.py
+++ bicicletada/test_csv.py
@@ -9,18 +9,18 @@ from csv import CSV
 
 class TestCSV(unittest.TestCase):
 
+    def setUp(self):
+        self.csv = CSV()
+
     def test_should_all_lines_have_same_quantity_fields(self):
-        csv = CSV()
-        field_number = len(csv.csv_table[0])
-        for line in csv.csv_table[1:]:
+        field_number = len(self.csv.csv_table[0])
+        for line in self.csv.csv_table[1:]:
             self.assertEquals(len(line), field_number)
 
     def test_first_line_should_have_column_names(self):
-        csv=CSV()
-        self.assertEquals(', '.join(csv.column_names), '"Estado", "Localização", "URL"')
+        self.assertEquals(', '.join(self.csv.column_names), '"Estado", "Localização", "URL"')
 
     def test_first_field_should_be_state(self):
-        csv=CSV()
         states = ['Acre', 'Alagoas', 'Amapá', 'Amazonas',
                   'Bahia', 'Ceará', 'Distrito Federal',
                   'Espírito Santo', 'Goiás', 'Maranhão',
@@ -30,17 +30,15 @@ class TestCSV(unittest.TestCase):
                   'Rio Grande do Norte', 'Rio Grande do Sul',
                   'Rondônia', 'Roraima', 'Santa Catarina',
                   'São Paulo', 'Sergipe', 'Tocantins']
-        for line in csv.csv_table[1:]:
+        for line in self.csv.csv_table[1:]:
             self.assertIn(line[0][1:-1], states)
 
     def test_last_field_should_be_url_http(self):
-        csv = CSV()
-        for line in csv.csv_table[1:]:
+        for line in self.csv.csv_table[1:]:
             self.assertRegexpMatches(line[2], r'^"http://www.bicicletada.org/')
 
     def test_should_save_csv_as_file(self):
-        csv = CSV()
-        csv.save_csv('XXXXXXXXXXXXXXXXX_my_csv_test_file.csv')
+        self.csv.save_csv('XXXXXXXXXXXXXXXXX_my_csv_test_file.csv')
         self.assertTrue(isfile('XXXXXXXXXXXXXXXXX_my_csv_test_file.csv'))
         remove('XXXXXXXXXXXXXXXXX_my_csv_test_file.csv')
     
