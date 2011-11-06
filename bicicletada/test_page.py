77e5b78f8b066de8b7973ac022728f009d94c177
Denis Costa <deniscostadsc@gmail.com>
2011-11-06 16:02:36 -0200

    Refatoração da classe de teste do page.

--- bicicletada/test_page.py
+++ bicicletada/test_page.py
@@ -5,14 +5,14 @@ import unittest
 from page import Page
 
 class TestPage(unittest.TestCase):
+    def setUp(self):
+        self.page = Page()
 
     def test_if_page_exist(self):
-        page=Page()
-        self.assertEquals(page.status_code, 200)
+        self.assertEquals(self.page.status_code, 200)
     
     def test_if_content_is_HTML(self):
-        page=Page()
-        self.assertEquals(page.content.endswith('</html>'), True)
+        self.assertEquals(self.page.content.endswith('</html>'), True)
 
 if __name__ == '__main__':
     unittest.main()
