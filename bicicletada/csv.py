488bea9cf15aeb3c99ed131d8dffe4340c4d9c72
Denis Costa <deniscostadsc@gmail.com>
2011-11-30 09:05:14 -0200

    Agora usando encapsulamento.

--- bicicletada/csv.py
+++ bicicletada/csv.py
@@ -7,13 +7,21 @@ from parser import Parser
 class CSV(object):
     #construtor
     def __init__(self):
-        self.column_names = ['"Estado"', '"Localização"', '"URL"']
+        self.__column_names = ['"Estado"', '"Localização"', '"URL"']
         # cria um objeto da classe parser "t" 
         t = Parser()
         #pega o html e devolve a tabela
-        self.csv_table = t.parse()
+        self.__csv_table = t.parse()
         #coloca no arquivo csv os nomes das colunas.
-        self.csv_table.insert(0, self.column_names)
+        self.__csv_table.insert(0, self.column_names)
+    
+    @property
+    def column_names(self):
+        return self.__column_names
+    
+    @property
+    def csv_table(self):
+        return self.__csv_table
     
     #Cria um novo arquivo e junta as linhas da tabela CSV quebrando as linhas.
     def save_csv(self, filename='grupos_bicicletada.csv'):
--- bicicletada/page.py
+++ bicicletada/page.py
@@ -7,17 +7,29 @@ class Page(object):
 
     #self indica que o atributo é do objeto, sem self é da classe    
     def __init__(self):        
-        self.url='http://bicicletada.org/'
+        self.__url='http://bicicletada.org/'
 
         # Pega a página configurada na URL.
-        page=urllib.urlopen(self.url)
+        page=urllib.urlopen(self.__url)
 
         # Retorna código de retorno HTTP
         # mais informação em:
         # http://pt.wikipedia.org/wiki/Hypertext_Transfer_Protocol#C.C3.B3digos_de_retorno
-        self.status_code=page.getcode()
+        self.__status_code=page.getcode()
 
         # Joga o conteúdo da página nesse atributo.
-        self.content=page.read()
+        self.__content=page.read()
         
         page.close()
+
+    @property
+    def url(self):
+        return self.__url
+    
+    @property
+    def status_code(self):
+        return self.__status_code
+
+    @property
+    def content(self):
+        return self.__content
